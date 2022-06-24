355baecb6f8d97e36b4487f3d0bbdcd717d50c4e
tgsk <opnj0517@gmail.com>
2022-06-24 22:12:11 +0900

    銃作成

---
+++ TeamC/Assets/Member/Togasaki/Scripts/Bullet.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class Bullet : MonoBehaviour
+{
+    /// <summary>
+    /// 弾のタイプ
+    /// </summary>
+    public int bulletType = 0;
+
+    /// <summary>
+    /// BulletManagerのObjectpool
+    /// </summary>
+    public ObjectPool<GameObject> objectPool;
+
+    /// <summary>
+    /// 動き終わって返却される際の場所
+    /// </summary>
+    public Transform poolPos;
+
+    /// <summary>
+    /// 弾の速さ
+    /// </summary>
+    public float bulletSpeed;
+
+    /// <summary>
+    /// 何かにあたった時
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        objectPool.Release(gameObject);
+    }
+
+    /// <summary>
+    /// 画面外に出た時リリース
+    /// </summary>
+    private void OnBecameInvisible()
+    {
+        objectPool.Release(gameObject);
+    }
+
+}
